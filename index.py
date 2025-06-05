e67721f63b46c6eb2f888d4f442f3b0375e45398
maxalva30 <max.alva@metso.com>
2025-06-04 23:49:28 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,25 +1,26 @@
-import dash
 from dash import dcc, html
-from dash.dependencies import Input, Output, State, MATCH, ALL
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 import io
 import base64
+from dash import Dash
 
 # Crear la aplicación Dash
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 server = app.server
 
-# Asegúrate de que el módulo 'plots' existe y funciona correctamente
-from pages import plots
+from pages import plots  # Importar la subpágina de análisis
 
 # Layout de la aplicación
 app.layout = html.Div(
     className="main-container",
+    style={'display': 'flex', 'flexDirection': 'column', 'minHeight': '100vh'},
     children=[
         # Contenedor del encabezado
         html.Div(
             className="header-container",
+            style={'display': 'flex', 'justify-content': 'center', 'alignItems': 'center', 'padding': '20px'},
             children=[
                 html.Img(src='/assets/MetsoLogo.png', className="logo"),
                 html.H1("Thickener Operational Data Analysis", className="main-title")
@@ -27,11 +28,10 @@ app.layout = html.Div(
         ),
         dcc.Location(id='url', refresh=False),
         html.Div(id='page-content', style={'flex': '1'}),
-        dcc.Store(id='stored-data', storage_type='session'),  # Aquí almacenaremos los datos leídos del archivo Excel
-        dcc.Store(id='upload-status', data='idle'),  # Nuevo Store para el estado de carga
-        html.Div(className='footer', children=[
-            html.P("Copyright © 2024 Metso")
-        ]),
+        dcc.Store(id='stored-data', storage_type='session'),
+        dcc.Store(id='project-name-store', storage_type='session'),
+        html.Div(className='footer', children=[html.P("Copyright © 2024 Metso")],
+                 style={'textAlign': 'center', 'padding': '10px'}),
         # Modal para la barra de progreso
         dbc.Modal(
             [
@@ -39,65 +39,24 @@ app.layout = html.Div(
                 dbc.ModalBody([
                     html.Div("Por favor espere mientras se carga el archivo."),
                     dbc.Progress(id="progress-bar", striped=True, animated=True, style={"marginTop": "10px"}),
+                    dcc.Interval(id="interval-progress", interval=500, n_intervals=0)
                 ]),
             ],
             id="loading-modal",
             is_open=False,
-            backdrop='static',  # Evita que se cierre al hacer clic fuera del modal
-            keyboard=False  # Evita que se cierre al presionar Esc
         ),
-        # Intervalo para actualizar la barra de progreso
-        dcc.Interval(id="interval-progress", interval=500, n_intervals=0, disabled=True),
     ]
 )
 
-# Callback para almacenar los datos del archivo cargado y actualizar el estado de carga
-@app.callback(
-    [Output('stored-data', 'data'),
-     Output('upload-status', 'data')],
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename'),
-    prevent_initial_call=True
-)
-def store_uploaded_data(contents, filename):
-    if contents is not None:
-        # Procesando el archivo
-        content_type, content_string = contents.split(',')
-        decoded = base64.b64decode(content_string)
-        try:
-            # Procesar el archivo Excel
-            df = pd.read_excel(io.BytesIO(decoded), sheet_name=0, skiprows=6, engine='openpyxl')
-
-            # Seleccionar las columnas deseadas
-            df = df.iloc[:, 3:14]  # Desde la columna D hasta la N
-
-            # Asegurarse de que la primera columna sea tratada como datetime
-            df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0], errors='coerce')
-
-            # Convertir el DataFrame a un diccionario para almacenarlo
-            data = df.to_dict('records')
-            # Actualizar estado a 'done'
-            upload_status = 'done'
-            return data, upload_status
-        except Exception as e:
-            print("Error al leer el archivo:", e)
-            # Actualizar estado a 'error'
-            upload_status = 'error'
-            return None, upload_status
-    else:
-        # Si no hay contenido, no actualizamos nada
-        return dash.no_update, dash.no_update
-
-# Callback para el enrutamiento de páginas
+# Callback para mostrar la página dependiendo del pathname
 @app.callback(
     Output('page-content', 'children'),
     Input('url', 'pathname')
 )
 def display_page(pathname):
     if pathname == '/plots':
-        return plots.layout  # Asegúrate de que 'plots.layout' está definido y funciona
+        return plots.layout
     else:
-        # Layout de la página principal (completo)
         return html.Div(
             className="content-container",
             children=[
@@ -150,7 +109,7 @@ def display_page(pathname):
                                 html.Table(
                                     children=[
                                         html.Tr([
-                                            html.Td(html.Label("Specific Gravity (-)"), className="label-cell"),
+                                            html.Td(html.Label("Solid Specific Gravity (-)"), className="label-cell"),
                                             html.Td(dcc.Input(type="number", id="specific-gravity", className="input-cell")),
                                         ]),
                                         html.Tr([
@@ -165,6 +124,7 @@ def display_page(pathname):
                         html.H3("Raw Data Entry", className="section-title"),
                         html.Div(
                             className="upload-container",
+                            style={'position': 'relative'},  # Para la "X"
                             children=[
                                 dcc.Upload(
                                     id='upload-data',
@@ -178,10 +138,14 @@ def display_page(pathname):
                                         'borderRadius': '5px',
                                         'textAlign': 'center',
                                         'backgroundColor': '#f9f9f9',
-                                        'cursor': 'pointer',
                                     },
                                     multiple=False
                                 ),
+                                # Botón "X" para eliminar archivo cargado, dentro del recuadro
+                                html.Button('×', id='remove-upload', n_clicks=0, style={
+                                    'position': 'absolute', 'top': '5px', 'right': '5px', 'backgroundColor': 'transparent',
+                                    'color': 'red', 'border': 'none', 'fontSize': '18px', 'cursor': 'pointer'
+                                }),
                                 html.Div(id='output-file-upload')
                             ]
                         ),
@@ -229,52 +193,57 @@ def display_page(pathname):
             style={'display': 'flex', 'flexDirection': 'row'}
         )
 
-# Callback para mostrar el nombre del archivo cargado
+# Callback para manejar la carga del archivo y eliminarlo
 @app.callback(
-    Output('upload-text', 'children'),
-    Input('upload-data', 'filename')
+    [Output('stored-data', 'data'), Output('upload-text', 'children')],
+    [Input('upload-data', 'contents'), Input('remove-upload', 'n_clicks')],
+    [State('upload-data', 'filename')]
 )
-def update_output_filename(filename):
-    if filename:
-        return html.Span([html.Img(src='/assets/excel-icon.png', style={'width': '20px', 'marginRight': '10px'}), f"{filename}"])
-    return "Drop or Select a File"
+def handle_uploaded_file(contents, remove_clicks, filename):
+    if remove_clicks > 0:
+        # Restablece el valor de n_clicks y permite cargar un nuevo archivo
+        return None, "Drop or Select a File"
 
-# Callback para manejar el modal y la barra de progreso
+    if contents is not None:
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        try:
+            df = pd.read_excel(io.BytesIO(decoded), sheet_name=0, skiprows=6, engine='openpyxl')
+            df = df.iloc[:, 3:14]  # Seleccionar las columnas desde la cuarta (D) hasta la décimo cuarta (N)
+            df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0], errors='coerce')  # Asegurarnos de que la primera columna sea datetime
+            return df.to_dict('records'), html.Span([html.Img(src='/assets/excel-icon.png', style={'width': '20px', 'marginRight': '10px'}), f"{filename}"])
+        except Exception as e:
+            print("Error al leer el archivo:", e)
+    return None, "Drop or Select a File"
+
+# Callback para manejar la barra de progreso y el modal
 @app.callback(
     [Output('loading-modal', 'is_open'),
      Output('progress-bar', 'value'),
-     Output('progress-bar', 'label'),
-     Output('interval-progress', 'disabled'),
-     Output('interval-progress', 'n_intervals')],
-    [Input('upload-data', 'filename'),
-     Input('upload-status', 'data'),
+     Output('progress-bar', 'label')],
+    [Input('upload-data', 'contents'),
      Input('interval-progress', 'n_intervals')],
-    [State('loading-modal', 'is_open'),
-     State('interval-progress', 'disabled')],
+    [State('loading-modal', 'is_open')],
     prevent_initial_call=True
 )
-def handle_upload_and_progress(filename, upload_status, n_intervals, is_open, interval_disabled):
-    ctx = dash.callback_context
-    if not ctx.triggered:
-        return is_open, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+def handle_upload_and_progress(contents, n_intervals, is_open):
+    if contents is not None and not is_open:
+        return True, 0, "0%"
+    if is_open:
+        progress = (n_intervals + 1) * 10
+        if progress >= 100:
+            return False, 100, "Carga completa"
+        return True, progress, f"{progress}%"
+    return is_open, 0, ""
 
-    triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
+# Callback para almacenar el valor de "Project Name" en dcc.Store
+@app.callback(
+    Output('project-name-store', 'data'),
+    Input('project-name', 'value')
+)
+def store_project_name(project_name):
+    return project_name if project_name else ""
 
-    if triggered_id == 'upload-data' and filename is not None:
-        # Abrir modal, habilitar intervalo y reiniciar contador
-        return True, 0, "0%", False, 0
-    elif triggered_id == 'interval-progress' and is_open:
-        if upload_status == 'done':
-            # Cerrar modal, deshabilitar intervalo
-            return False, 100, "Carga completa", True, n_intervals
-        else:
-            # Incrementar progreso más lentamente
-            progress = min(n_intervals * 5, 95)  # Incrementos de 5% hasta el 95%
-            return True, progress, f"{progress}%", False, n_intervals
-    elif triggered_id == 'upload-status' and upload_status == 'done' and is_open:
-        # Asegurar que el progreso llegue al 100% y cerrar el modal
-        return False, 100, "Carga completa", True, n_intervals
-    return is_open, dash.no_update, dash.no_update, interval_disabled, n_intervals
-
+# Ejecutar la aplicación
 if __name__ == "__main__":
     app.run_server(debug=True)
